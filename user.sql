a8313ad23cfeaa5028dadf464ea028264aafab8d
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2021-03-07 21:20:00 +0700

    Update dolt database

---
+++ user.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `user` (`id`) VALUES (1);
+INSERT INTO `user` (`id`) VALUES (2);
+INSERT INTO `user` (`id`) VALUES (3);
+INSERT INTO `user` (`id`) VALUES (4);
+INSERT INTO `user` (`id`) VALUES (5);
+INSERT INTO `user` (`id`) VALUES (6);
+INSERT INTO `user` (`id`) VALUES (7);
+INSERT INTO `user` (`id`) VALUES (8);
+INSERT INTO `user` (`id`) VALUES (9);
